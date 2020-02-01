09cb06ea05da2a2153f802dd649c9807826ef6d0
Jason Le <le.kent.jason@gmail.com>
2020-02-01 11:05:27 -0500

    Az/create base app list (#21)
    * init
    
    * Connected Firebase (#17)
    
    * added firestore
    
    * updated .gitignore to do not allow credentials file
    
    * cleanup code and warnings
    
    Co-authored-by: Arryan Zaman <arryan.zaman@hotmail.com>
    
    Co-authored-by: Arryan Zaman <arryan.zaman@hotmail.com>

---
+++ app/src/main/java/com/example/pinatlas/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.pinatlas
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pinatlas.adapter.PlaceAdapter
+import com.example.pinatlas.model.Place
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+
+class MainActivity : AppCompatActivity(), PlaceAdapter.OnPlaceSelectedListener {
+    private var TAG = MainActivity::class.java.simpleName
+
+    private val mFirestore: FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
+
+    private val mQuery: Query by lazy { mFirestore.collection("places") }
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: PlaceAdapter
+    private lateinit var context: Context
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        try {
+            context = this;
+            recyclerView = findViewById(R.id.itineraryRecyclerView)
+            recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+
+            adapter = PlaceAdapter(mQuery, this)
+
+            recyclerView.adapter = adapter
+
+        } catch (e: Exception) {
+            Log.e(TAG, e.message)
+        }
+    }
+
+    override fun onPlaceSelected(place: DocumentSnapshot) {
+        var snapshot = place.toObject(Place::class.java)
+        Toast.makeText(context, "Clicked on " + snapshot!!.name, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
+    }
+}
---
+++ app/src/main/java/com/example/pinatlas/model/Place.kt
@@ -0,0 +1,21 @@
+package com.example.pinatlas.model
+
+import com.google.firebase.firestore.IgnoreExtraProperties;
+import com.google.firebase.firestore.ServerTimestamp
+import java.util.*
+
+@IgnoreExtraProperties
+class Place {
+    var name: String? = null
+    var placeId: String? = null
+
+    @ServerTimestamp
+    var timestamp: Date? = null
+
+    constructor()
+
+    constructor(name: String, placeId: String) {
+        this.name = name
+        this.placeId = placeId
+    }
+}
