76cc39eb6044c35a952b9daffed65090f39524df
MonicaVu <monica.vu.005@gmail.com>
2020-02-02 18:27:33 -0500

    created draft ui

---
+++ app/src/main/java/com/example/pinatlas/GoogleLogin.kt
@@ -0,0 +1,12 @@
+package com.example.pinatlas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoogleLogin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google_login)
+    }
+}
