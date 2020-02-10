d58a9592eb6a7066d2cd5d902d99c3d52cfe269f
Jason Le <le.kent.jason@gmail.com>
2020-02-09 23:32:54 -0500

    created travel dash and itinerary view (#47)
    * init
    
    * Connected Firebase (#17)
    
    * added firestore
    
    * updated .gitignore to do not allow credentials file
    
    * cleanup code and warnings
    
    Co-authored-by: Arryan Zaman <arryan.zaman@hotmail.com>
    
    * something cool is added
    
    * something cool is removed
    
    * Mapbox included into the travel dash
    
    * get places api working
    
    * Mapbox with your location added (personalise the code to suit needs)
    
    * Horizontal Recycler View for Travel Dash - waiting for proper data
    Will be polished soon!
    
    * New UI Components Added - Recycler Views Need to be added!
    
    * New UI Components Added
    
    * Merge branch 'master' into ss/project_base
    
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/strings.xml
    
    Co-authored-by: Arryan Zaman <arryan.zaman@hotmail.com>
    Co-authored-by: Shubham Sharan <32628481+ShubhamSharan@users.noreply.github.com>

---
+++ app/src/main/java/com/example/pinatlas/TravelDash.kt
@@ -0,0 +1,157 @@
+package com.example.pinatlas
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.pinatlas.adapter.Past_TravelDash_Adapter
+import com.example.pinatlas.adapter.Upcom_TravelDash_Adapter
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
+import com.takusemba.multisnaprecyclerview.MultiSnapRecyclerView
+
+class TravelDash : AppCompatActivity() , OnMapReadyCallback, PermissionsListener {
+
+    private lateinit var mapView : MapView
+    private lateinit var mapboxMap: MapboxMap
+    private var permissionsManager: PermissionsManager = PermissionsManager(this)
+
+    private val TILES = Array(1000) { index -> "Item $index" }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Mapbox.getInstance(applicationContext, getString(R.string.mapbox_access_token))
+        setContentView(R.layout.traveldash)
+
+        //For the Mapbox Implementation
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
+        //Local the tiles for past/upcoming trips
+        val pastAdapter = Past_TravelDash_Adapter(TILES)
+        val pastRecyclerView = findViewById<MultiSnapRecyclerView>(R.id.PTview)
+        val pastManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        pastRecyclerView.layoutManager = pastManager
+        pastRecyclerView.adapter = pastAdapter
+
+        val upcomAdapter = Upcom_TravelDash_Adapter(TILES)
+        val upcomRecyclerView = findViewById<MultiSnapRecyclerView>(R.id.UTView)
+        val upcomManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        upcomRecyclerView.layoutManager = upcomManager
+        upcomRecyclerView.adapter = upcomAdapter
+        }
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.mapboxMap = mapboxMap
+        mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+            enableLocationComponent(it)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableLocationComponent(loadedMapStyle: Style) {
+// Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+// Create and customize the LocationComponent's options
+            val customLocationComponentOptions = LocationComponentOptions.builder(this)
+                .trackingGesturesManagement(true)
+                .accuracyColor(ContextCompat.getColor(this, R.color.mapbox_blue))
+                .build()
+
+            val locationComponentActivationOptions = LocationComponentActivationOptions.builder(this, loadedMapStyle)
+                .locationComponentOptions(customLocationComponentOptions)
+                .build()
+
+        // Get an instance of the LocationComponent and then adjust its settings
+            mapboxMap.locationComponent.apply {
+
+                // Activate the LocationComponent with options
+                activateLocationComponent(locationComponentActivationOptions)
+
+        // Enable to make the LocationComponent visible
+                isLocationComponentEnabled = true
+
+        // Set the LocationComponent's camera mode
+                cameraMode = CameraMode.TRACKING
+
+        // Set the LocationComponent's render mode
+                renderMode = RenderMode.COMPASS
+            }
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(this)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: List<String>) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show() //Need this is to be removed
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            enableLocationComponent(mapboxMap.style!!)
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show() //Need this is to be removed
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+}
+
+
+
+
---
+++ app/src/main/java/com/example/pinatlas/adapter/Past_TravelDash_Adapter.kt
@@ -0,0 +1,37 @@
+package com.example.pinatlas.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.pinatlas.R
+
+
+class Past_TravelDash_Adapter(
+    private val titles: Array<String>
+) : androidx.recyclerview.widget.RecyclerView.Adapter<Past_TravelDash_Adapter.ViewHolder>() {
+
+  override fun onCreateViewHolder(
+      viewGroup: ViewGroup,
+      viewType: Int
+  ): ViewHolder {
+    val inflater = LayoutInflater.from(viewGroup.context)
+    val view = inflater.inflate(R.layout.traveldash_item, viewGroup, false)
+    return ViewHolder(view)
+  }
+
+  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    val title = titles[position]
+
+    holder.title.text = title
+  }
+
+  override fun getItemCount(): Int {
+    return titles.size
+  }
+
+  class ViewHolder(itemView: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(
+      itemView) {
+    val title: TextView = itemView.findViewById(R.id.location) as TextView
+  }
+}
