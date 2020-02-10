069d9d38d51dc27b3db79f2b7fcb87ea0a50acdd
Jason Le <le.kent.jason@gmail.com>
2020-02-09 23:08:20 -0500

    Revert "created travel dash and itinerary view (#45)" (#46)
    This reverts commit 17c4e1ae63e735c271d0bf2cfdfc1fb8998c4d01.

---
+++ app/src/main/java/com/example/pinatlas/TravelBoard.kt
@@ -0,0 +1,12 @@
+package com.example.pinatlas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TravelBoard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_travel_board)
+    }
+}
