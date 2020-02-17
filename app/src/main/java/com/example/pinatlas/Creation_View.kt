0e5bb0da57a2d80bc8531bc50e11e2781a4d3197
Arryan <arryan.zaman@hotmail.com>
2020-02-16 22:11:03 -0500

    create adapter class

--- app/src/main/java/com/example/pinatlas/Creation_View.kt
+++ app/src/main/java/com/example/pinatlas/Creation_View.kt
@@ -25,6 +25,7 @@ import android.view.ViewGroup
 import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.pinatlas.adapter.ActivityListAdapter
 import com.google.android.gms.common.api.Status
 import com.google.android.libraries.places.api.model.Place
 import com.google.firebase.firestore.Query
@@ -167,32 +168,5 @@ class Creation_View : AppCompatActivity() {
 
     fun updateData() {
         tripDocument.set(trip)
-    }
-}
-
-class ActivityListAdapter ( private val places: ArrayList<Place>
-) : RecyclerView.Adapter<ActivityListAdapter.ViewHolder>() {
-
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val inflater = LayoutInflater.from(viewGroup.context)
-        val view = inflater.inflate(R.layout.item_creation_list_tile, viewGroup, false)
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.activity.text = places[position].name
-        holder.address.text = places[position].address
-        holder.priority.text = "#${position+1}"
-    }
-
-    override fun getItemCount(): Int {
-        return places.size
-    }
-
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(
-        itemView) {
-        val activity: TextView = itemView.findViewById(R.id.activityName) as TextView
-        val address: TextView = itemView.findViewById(R.id.activityAddress) as TextView
-        val priority: TextView = itemView.findViewById(R.id.activityPriority) as TextView
     }
 }
