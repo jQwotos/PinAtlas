8d04b924a4ac5da41724e2e646e4150d7626816b
Shubham Sharan <shubhamsharan@outlook.com>
2020-03-01 19:20:59 -0500

    'Mapbox broken but fatal code added'

--- app/src/main/java/com/example/pinatlas/ItineraryView.kt
+++ app/src/main/java/com/example/pinatlas/ItineraryView.kt
@@ -2,14 +2,13 @@ package com.example.pinatlas
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.ColorUtils
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -22,8 +21,10 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.geojson.Feature
+import com.mapbox.geojson.FeatureCollection
+import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
 import com.mapbox.mapboxsdk.location.LocationComponentOptions
 import com.mapbox.mapboxsdk.location.modes.CameraMode
@@ -32,13 +33,17 @@ import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
-import com.mapbox.mapboxsdk.plugins.annotation.FillOptions
-import com.mapbox.mapboxsdk.plugins.annotation.FillManager
+import com.mapbox.mapboxsdk.plugins.annotation.Fill
+import com.mapbox.mapboxsdk.plugins.markerview.MarkerView
+import com.mapbox.mapboxsdk.plugins.markerview.MarkerViewManager
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
 import com.takusemba.multisnaprecyclerview.MultiSnapRecyclerView
 
-class ItineraryView : AppCompatActivity() , OnMapReadyCallback, PermissionsListener {
+class ItineraryView : AppCompatActivity() , OnMapReadyCallback, PermissionsListener{
     private val TAG = ItineraryView::class.java
+
     private val context: Context = this
 
     private lateinit var mapView : MapView
@@ -46,7 +51,11 @@ class ItineraryView : AppCompatActivity() , OnMapReadyCallback, PermissionsListe
     private lateinit var tripName: TextView
     private lateinit var viewModel: CreationViewModel
     private lateinit var tripId: String
-    private lateinit var fillManager: FillManager
+    private lateinit var markerViewManager : MarkerViewManager
+    private lateinit var adapter: ActivityListAdapter
+    private lateinit var marker: MarkerView
+
+    private lateinit var  fill : Fill
 
     private var permissionsManager: PermissionsManager = PermissionsManager(this)
     private val currentUser: FirebaseUser? by lazy { FirebaseAuth.getInstance().currentUser }
@@ -72,7 +81,7 @@ class ItineraryView : AppCompatActivity() , OnMapReadyCallback, PermissionsListe
         })
 
         //Local the tiles for past/upcoming trips
-        val adapter = ActivityListAdapter(viewModel, ViewModes.ITINERARY_MODE)
+        adapter = ActivityListAdapter(viewModel, ViewModes.ITINERARY_MODE)
         val submitListView = findViewById<MultiSnapRecyclerView>(R.id.sublist_recycler_view)
         val manager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         submitListView.layoutManager = manager
@@ -87,26 +96,58 @@ class ItineraryView : AppCompatActivity() , OnMapReadyCallback, PermissionsListe
 
     override fun onMapReady(mapboxMap: MapboxMap) {
         this.mapboxMap = mapboxMap
-        mapboxMap.setStyle(Style.MAPBOX_STREETS) {
-            enableLocationComponent(it)
-//            val placesList = viewModel.tripPlaces.value
-//            val inLatLngs = ArrayList<LatLng>()
-//
-//            for(n in 0 until placesList!!.size){
-//                inLatLngs.add(LatLng(placesList[n].coordinates!!.latitude, placesList[n].coordinates!!.longitude))
-//            }
-//            val latLngs = ArrayList<List<LatLng>>()
-//            latLngs.add(inLatLngs)
-//
-//            val fillOptions = FillOptions()
-//                .withLatLngs(latLngs)
-//            fillManager?.create(fillOptions)
-//
-//            // random add fills across the globe
-//            val fillOptionsList = ArrayList<FillOptions>()
-//            fillManager?.create(fillOptionsList)
-        }
+        mapView = findViewById(R.id.mapView)
+        viewModel.tripPlaces.observe(this, Observer { placesList ->
+            if (placesList != null) {
+                val inLatLngs : ArrayList<Feature> = ArrayList<Feature>()
+
+                for (n in 0 until placesList.size) {
+                    inLatLngs.add(  Feature.fromGeometry(
+                        Point.fromLngLat(placesList[n].coordinates!!.latitude,placesList[n].coordinates!!.longitude))
+                    )
+                }
+
+
+
+                mapboxMap.setStyle(
+                    Style.Builder().fromUri("mapbox://styles/mapbox/cjf4m44iw0uza2spb3q0a7s41")
+
+                        // Add the SymbolLayer icon image to the map style
+                        .withImage(ICON_ID, BitmapFactory.decodeResource(
+                            this.getResources(), R.drawable.common_full_open_on_phone))
+                        // Adding a GeoJson source for the SymbolLayer icons.
+                        .withSource(
+                            GeoJsonSource(
+                                SOURCE_ID,
+                                FeatureCollection.fromFeatures(inLatLngs)
+                            )
+                        )
+
+                        // Adding the actual SymbolLayer to the map style. An offset is added that the bottom of the red
+                        // marker icon gets fixed to the coordinate, rather than the middle of the icon being fixed to
+                        // the coordinate point. This is offset is not always needed and is dependent on the image
+                        // that you use for the SymbolLayer icon.
+                        .withLayer(
+                            SymbolLayer(LAYER_ID, SOURCE_ID)
+                                .withProperties(
+                                    PropertyFactory.iconAllowOverlap(true),
+                                    PropertyFactory.iconOffset(arrayOf(0f, -9f)),
+                                    PropertyFactory.iconSize(100f)
+                                )
+                        )
+                ) {
+                    enableLocationComponent(it)
+                }
+
+
+
+
+            }
+        })
+
     }
+
+
 
     private fun enableLocationComponent(loadedMapStyle: Style) {
         // Check if permissions are enabled and if not request
@@ -189,6 +230,7 @@ class ItineraryView : AppCompatActivity() , OnMapReadyCallback, PermissionsListe
 
     override fun onDestroy() {
         super.onDestroy()
+        markerViewManager?.onDestroy()
         mapView.onDestroy()
     }
 
@@ -201,6 +243,13 @@ class ItineraryView : AppCompatActivity() , OnMapReadyCallback, PermissionsListe
         val intent = Intent(this, CreationView::class.java)
         intent.putExtra(Constants.TRIP_ID.type, tripId)
         startActivity(intent)
+    }
+    companion object {
+
+        // Create names for the map's source, icon, and layer IDs.
+        private val SOURCE_ID = "SOURCE_ID"
+        private val ICON_ID = "ICON_ID"
+        private val LAYER_ID = "LAYER_ID"
     }
 
 }
