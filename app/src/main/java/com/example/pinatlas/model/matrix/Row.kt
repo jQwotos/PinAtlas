8e76debe8d40b451f399432b7310ce475820c7fd
Jason Le <le.kent.jason@gmail.com>
2020-04-03 01:36:39 -0400

    null checks make me sad
    Signed-off-by: Jason Le <le.kent.jason@gmail.com>

--- app/src/main/java/com/example/pinatlas/model/matrix/Row.kt
+++ app/src/main/java/com/example/pinatlas/model/matrix/Row.kt
@@ -2,4 +2,8 @@ package com.example.pinatlas.model.matrix
 
 class Row {
     var elements: ArrayList<Element> = ArrayList()
+
+    constructor(elements: ArrayList<Element>) {
+        this.elements = elements
+    }
 }
